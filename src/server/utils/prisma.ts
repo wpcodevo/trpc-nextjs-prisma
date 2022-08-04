b772f5c6869fbafd26de3e5fe5799504d4f3ab8a
Edem <ziddah.edem@gmail.com>
2022-08-04 10:49:37 +0000

    updated

--- src/server/utils/prisma.ts
+++ src/server/utils/prisma.ts
@@ -9,3 +9,17 @@ export const prisma = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
 }
+
+async function connectDB() {
+  try {
+    await prisma.$connect();
+    console.log('🚀 Database connected successfully');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export default connectDB;
