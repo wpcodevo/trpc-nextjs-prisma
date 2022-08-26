27a2dc0253b4184c15e3bfd2982292cd2d930b1e
Edem Ziddah <contact@codevoweb.com>
2022-08-26 06:56:26 +0000

    updated

--- src/client/components/modals/post.modal.tsx
+++ src/client/components/modals/post.modal.tsx
@@ -19,7 +19,7 @@ const PostModal: FC<IPostModal> = ({
         className='fixed inset-0 bg-[rgba(0,0,0,.5)] z-[1000]'
         onClick={() => setOpenPostModal(false)}
       ></div>
-      <div className='max-w-lg w-full rounded-md fixed top-0 left-1/2 -translate-x-1/2 bg-white z-[1001] p-6'>
+      <div className='max-w-lg w-full rounded-md fixed top-0 xl:top[10%] left-1/2 -translate-x-1/2 bg-white z-[1001] p-6'>
         {children}
       </div>
     </>,
