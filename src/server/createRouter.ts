46318f184ba7aba2d73542bfe75a75c294306c6b
Edem Ziddah <contact@codevoweb.com>
2022-10-30 16:53:15 +0000

    updated

--- src/server/createRouter.ts
+++ src/server/createRouter.ts
@@ -1,7 +1,19 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { Context } from "./createContext";
 
 export const t = initTRPC.context<Context>().create({
   transformer: superjson,
 });
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to access this resource",
+    });
+  }
+  return next();
+});
+
+export const protectedProcedure = t.procedure.use(isAuthed);
--- src/server/routers/user.routes.ts
+++ src/server/routers/user.routes.ts
@@ -1,18 +1,5 @@
-import { t } from "../createRouter";
-import * as trpc from "@trpc/server";
+import { protectedProcedure, t } from "../createRouter";
 import { getMeHandler } from "../controllers/user.controller";
-
-const isAuthed = t.middleware(({ next, ctx }) => {
-  if (!ctx.user) {
-    throw new trpc.TRPCError({
-      code: "UNAUTHORIZED",
-      message: "You must be logged in to access this resource",
-    });
-  }
-  return next();
-});
-
-export const protectedProcedure = t.procedure.use(isAuthed);
 
 const userRouter = t.router({
   getMe: protectedProcedure.query(({ ctx }) => getMeHandler({ ctx })),
